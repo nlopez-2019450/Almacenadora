b66f35dc964ef81dfed187a319232b9748790ad3
Daniel <dgonzalez-2019360@kinal.edu.gt>
2023-04-21 15:57:39 -0600

    Se edito el área de .env

---
+++ src/models/tarea.model.js
@@ -0,0 +1,32 @@
+"use strict"
+var mongoose= require("mongoose")
+var Schema = mongoose.Schema;
+
+const tareaSchema = Schema({
+    tareaNombre:{
+        type: String,
+        required: true
+    },
+    descripcion:{
+        type: String,
+        required: true
+    },
+    fechaInicio:{
+        type: String,
+        required: true
+    },
+    fechaCierre:{
+        type: String,
+        required: true
+    },
+    estado:{
+        type: String,
+        required: true
+    },
+    nombreCompleto:{
+        type:String,
+        required:true
+    }
+});
+
+module.exports = mongoose.model('Tareas', tareaSchema);
